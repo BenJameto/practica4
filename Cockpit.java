93911d14a7fe0484c4c72c6c5bf718baa1802c50
BenJameto <martinez.benjaminosorio@gmail.com>
2024-03-24 12:34:15 -0600

    se agregaron las clases PropulsionSystem, Armor, Cockpit y Weapon que son los componentes de la nave espacial

---
+++ Cockpit.java
@@ -0,0 +1,43 @@
+public class Cockpit {
+    private String name;
+    private String description;
+    private double price;
+
+    public Cockpit(String name, String description, double price) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Cockipt\n" +
+                "name: " + name + "\n" +
+                "description: " + description + "\n" +
+                "price: " + price + "\n";
+    }
+}
---
+++ Spaceship.java
@@ -0,0 +1,48 @@
+public class Spaceship {
+    private PropulsionSystem propulsionSystem;
+    private Armor armor;
+    private Cockpit cockpit;
+    private Weapon weapon;
+
+    public PropulsionSystem getPropulsionSystem() {
+        return propulsionSystem;
+    }
+
+    public void setPropulsionSystem(PropulsionSystem propulsionSystem) {
+        this.propulsionSystem = propulsionSystem;
+    }
+
+    public Armor getArmor() {
+        return armor;
+    }
+
+    public void setArmor(Armor armor) {
+        this.armor = armor;
+    }
+
+    public Cockpit getCockpit() {
+        return cockpit;
+    }
+
+    public void setCockpit(Cockpit cockpit) {
+        this.cockpit = cockpit;
+    }
+
+    public Weapon getWeapon() {
+        return weapon;
+    }
+
+    public void setWeapon(Weapon weapon) {
+        this.weapon = weapon;
+    }
+
+    @Override
+    public String toString() {
+        return "Spaceship{" +
+                "propulsionSystem=" + propulsionSystem +
+                ", armor=" + armor +
+                ", cockpit=" + cockpit +
+                ", weapon=" + weapon +
+                '}';
+    }
+}
